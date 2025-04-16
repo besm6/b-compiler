e785075223aedc7c92389242da3831950da383be
Serge <serge.vakulenko@gmail.com>
2025-04-15 23:36:57 -0700

    Enable native test.

--- besm6/tests/compiler_test.cpp
+++ besm6/tests/compiler_test.cpp
@@ -2,8 +2,7 @@
 
 #include "fixture.h"
 
-// Not working yet: need KOI7 encoding in read().
-TEST_F(besm6, DISABLED_native_empty_program)
+TEST_F(besm6, native_empty_program)
 {
     auto const b_besm = file_contents(TEST_DIR "/../b-besm.b");
     auto output = compile_and_run_with_input(b_besm, R"(
@@ -13,6 +12,9 @@ TEST_F(besm6, DISABLED_native_empty_program)
     )");
     const std::string expect = R"( MAIN:8,NAME,
  PROGRAM:,ENTRY,
+   8,VTM,20000B
+   9,VTM,40000B
+  10,VTM,60000B
  MAIN:,BSS,
  B/SAVE0:,SUBP,
  B/RET:,SUBP,
